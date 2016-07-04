c04e399ecbdbb707b3d4b7c410ecda967be2f3a4
Christopher Bartling <chris.bartling@gmail.com>
2016-07-04 01:28:07 -0500

    Getting everything configured for Auth0 integration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+    providers: [AuthService],
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+
+    constructor(private authService:AuthService) {
+    }
 }
